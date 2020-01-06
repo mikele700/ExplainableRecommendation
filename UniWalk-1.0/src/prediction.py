6d77a9b454aa0982610b34924330a37bcb5046d0
unknown <mi.pommella@studenti.unina.it>
2020-01-06 20:20:04 +0100

    Overlay graph

--- UniWalk-1.0/src/prediction.py
+++ UniWalk-1.0/src/prediction.py
@@ -24,6 +24,7 @@ Import packages
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 import numpy as np
 import pandas as pd
+import random
 
 
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
@@ -56,6 +57,11 @@ def prediction(args, numfolds):
 		biases = np.loadtxt(bias_filename)
 		id_to_th = {j: i for i, j in enumerate(entities)}
         
+        # Classification
+		profile = list()
+		for i in range(args.max_u_id + 1):
+		    profile.append(random.randint(0,4))
+        
 		# Dr: set of the highest rated entities
 		p_graph = args.graphpath + "p_graph_%s_%d.txt" % (args.graphparas, fold)
 		df = pd.read_csv(p_graph, delimiter='\t', names=['e1', 'e2', 'w'])
@@ -65,7 +71,14 @@ def prediction(args, numfolds):
 		for i, j, z in zip(df.e1, df.e2, df.w):
 		    if z > (maxR * 2 / 3):
 		        Dr[i].append(j)
-        
+                
+        # Profile's preferences
+		items_profile = list()
+		for i in range(args.max_i_id + 1 - args.min_i_id):
+		    items_profile.append([0, 0, 0, 0, 0])
+		    for j in range(len(Dr[i + args.min_i_id])):
+		    	items_profile[i][profile[Dr[i + args.min_i_id][j]]] += 1
+            
         # Dp: set of the similar entities
 		Dp_filename = args.learningpath + "Dp_" + args.learningparas + "_%d.txt" % fold
 		Dp_file = open(Dp_filename,"r")
@@ -80,7 +93,6 @@ def prediction(args, numfolds):
 		top_users = list()
 		top_user_similarities = list()
 		friends = list()
-		max_similarity = 0
 
 
 		# Predict ratings in test set
@@ -93,7 +105,6 @@ def prediction(args, numfolds):
 			top_3_item_similarities = list()
 			top_3_user_similarities = list()
 			friends_liked = list()
-			loacal_max = 0
             
 
 			# If both user and item are not learned in train
@@ -116,7 +127,7 @@ def prediction(args, numfolds):
 				b_i = biases[i_th]
                 
                 # Get explaination
-				top_3_items, top_3_item_similarities, friends_liked, top_3_users, top_3_user_similarities, local_max = explain(Dr, Dp, user, item, args.min_u_id, args.max_u_id)
+				top_3_items, top_3_item_similarities, friends_liked, top_3_users, top_3_user_similarities = explain(Dr, Dp, user, item, args.min_u_id, args.max_u_id)
 
 			# If only user is learned in train
 			elif (user in entities):
@@ -151,27 +162,22 @@ def prediction(args, numfolds):
 			top_users.append(top_3_users)
 			top_user_similarities.append(top_3_user_similarities)
 			friends.append(friends_liked)
-			if local_max > max_similarity:
-				max_similarity = local_max
 			
 			
 		# 4. Calculate errors
 		Test_MAEs[fold] = np.mean(np.abs(prediction - test.r))
 		Test_RMSEs[fold] = np.sqrt(np.mean((prediction - test.r) ** 2))
         
-		#threshold = max_similarity * 2 / 3
         
 		# 5. Save the prediction results
 		pred_filename = "../data/%s/prediction/pred_%d.txt" % (args.dataset, fold)
 		with open(pred_filename, mode='w') as f:
 			for u, i, p, ti, tis, tu, tus, fr in zip(test.u, test.i, prediction, top_items, top_item_similarities, top_users, top_user_similarities, friends):
-				f.write("%d\t%d\t%.5f" % (u, i, p))
+				f.write("%d\t%d\t%.5f\t%d" % (u, i, p, items_profile[int(i) - args.min_i_id].index(max(items_profile[int(i) - args.min_i_id]))))
 				for j in range(3):
 					if len(ti) > j:
-						#if tis[j] > threshold:
 						f.write("\ti%d: %d\t%.5f" % (j, ti[j], tis[j]))
 					if len(tu) > j:
-						#if tus[j] > threshold:
 						f.write("\tu%d: %d\t%.5f" % (j, tu[j], tus[j]))
 				for j in range(len(fr)):
 					f.write("\tf%d: %d" % (j, fr[j]))
@@ -196,7 +202,6 @@ def explain(Dr, Dp, target_user, target_item, min_u_id, max_u_id):
     top_3_user_similarities = list()
     users = list()
     friends_liked = list()
-    max_similarity = 0
     
     # Search of the user's highest rated items. Evaluate their similarity with the target item
     e_rated = Dr[target_user]
@@ -209,17 +214,15 @@ def explain(Dr, Dp, target_user, target_item, min_u_id, max_u_id):
             similarity.append(2 * e_similar.count(e_rated[i]) / (len(e_similar) + len(Dp[e_rated[i]])))
             
     if len(items) > 0:
-        max_similarity = max(similarity)
-        max_S = max_similarity
         for i in range(3):
-            max_index = similarity.index(max_S)
+            max_similarity = max(similarity)
+            max_index = similarity.index(max_similarity)
             top_3_items.append(items[max_index])
-            top_3_item_similarities.append(max_S)
+            top_3_item_similarities.append(max_similarity)
             items.pop(max_index)
             similarity.pop(max_index)
             if len(items) == 0:
                 break
-            max_S = max(similarity)
     
     # Searching of users who liked the target item. Evaluate their similarity with the target user        
     e_rated = Dr[target_item]
@@ -233,20 +236,17 @@ def explain(Dr, Dp, target_user, target_item, min_u_id, max_u_id):
             similarity.append(2 * e_similar.count(e_rated[i]) / (len(e_similar) + len(Dp[e_rated[i]])))
     
     if len(users) > 0:     
-        max_S = max(similarity)
-        if max_S > max_similarity:
-            max_similarity = max_S
         for i in range(3):
-            max_index = similarity.index(max_S)
+            max_similarity = max(similarity)
+            max_index = similarity.index(max_similarity)
             top_3_users.append(users[max_index])
-            top_3_user_similarities.append(max_S)
+            top_3_user_similarities.append(max_similarity)
             users.pop(max_index)
             similarity.pop(max_index)
             if len(users) == 0:
                 break
-            max_S = max(similarity)
     
     
-    return top_3_items, top_3_item_similarities, friends_liked, top_3_users, top_3_user_similarities, max_similarity
+    return top_3_items, top_3_item_similarities, friends_liked, top_3_users, top_3_user_similarities
 
 
